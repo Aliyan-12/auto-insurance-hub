7eae762bc9d13c8d0796f7a69840ad25cbce7921
Aliyan-12 <alyannasir225@gmail.com>
2024-09-26 20:44:45 +0500

    save data to db.

--- php/save.php
+++ php/save.php
@@ -1,6 +1,5 @@
 <?php
 
-print_r($_GET);exit;
 $servername = "localhost";
 $dbname = "u129255652_autosecurehub";
 $username = "u129255652_db_user";
@@ -16,9 +15,13 @@ if ($conn->connect_error) {
 
 // Get data from URL (GET request)
 // Example: your-url.php?name=John&age=30&email=john@example.com
-$fname = isset($_GET['name']) ? $_GET['name'] : '';
-$age = isset($_GET['age']) ? (int)$_GET['age'] : 0;
-$email = isset($_GET['email']) ? $_GET['email'] : '';
+$fname = $_GET['fname'];
+$lname = $_GET['lname'];
+$email = $_GET['email'];
+$phone = $_GET['phone'];
+$zip_code = $_GET['zip_code'];
+
+print_r($fname, $lname, $email, $phone, $zip_code);exit;
 
 $stmt = $conn->prepare("INSERT INTO leads (name, age, email) VALUES (?, ?, ?)");
 $stmt->bind_param("sis", $name, $age, $email); // "sis" means string, integer, string
