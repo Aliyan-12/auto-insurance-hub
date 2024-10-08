d99ec50674f03ed9c1d3b0b6c4548e7e00b3d0f4
Aliyan-12 <alyannasir225@gmail.com>
2024-10-09 02:26:23 +0500

    partners added

---
+++ pages/partners.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <title>Partners - Auto Insurance</title>
+
+    <!-- Fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Raleway:400,700,800,900" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400" rel="stylesheet">
+    <link rel="stylesheet" href="css/font-awesome.min.css">
+
+    <!-- Bootstrap -->
+    <link rel="stylesheet" href="../css/bootstrap.min.css">
+    <link rel="stylesheet" href="../css/style.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+</head>
+
+<body>
+    <?php include 'layout/navbar.html' ?>
+
+    <section class="reviews">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-offset-1 col-md-10">
+                    <div class="reviews-title">
+                        <h2>Our Partners</h2>
+                        <div class="d-flex gap-4">
+                            <p class="text-start"><strong>1 . Progressive</strong></p>
+                        </div>
+                        <div class="d-flex gap-4">
+                            <p class="text-start"><strong>2 . Allstate</strong></p>
+                        </div>
+                        <div class="d-flex gap-4">
+                            <p class="text-start"><strong>3 . Nationwide</strong></p>
+                        </div>
+                        <div class="d-flex gap-4">
+                            <p class="text-start"><strong>4 . Liberty Mutual</strong></p>
+                        </div>
+                        <div class="d-flex gap-4">
+                            <p class="text-start"><strong>5 . 21st Century</strong></p>
+                        </div>
+                        <div class="d-flex gap-4">
+                            <p class="text-start"><strong>6 . Hippo</strong></p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <?php include 'layout/footer.html' ?>
+
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="js/jquery-3.2.1.min.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="js/bootstrap.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz"
+        crossorigin="anonymous"></script>
+    <!-- Bootstrap Validator-->
+    <script type="text/javascript" src="js/bootstrapValidator.min.js"></script>
+    <script src="js/custom.js"></script>
+</body>
+
+</html>
